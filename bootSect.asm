f39b9c82a7d7cbdaa90b22daee5213bc006e6dd3
ShivamPR21 <pandeyshivam2023robotics@gmail.com>
2023-11-17 00:22:24 +0530

    Working code with first intro of kernel.
    Signed-off-by: ShivamPR21 <pandeyshivam2023robotics@gmail.com>

--- bootSect.asm
+++ bootSect.asm
@@ -3,16 +3,23 @@
 ;;
         org 0x7c00              ; Indicated where Boot code starts; makes sure adresses don't change
 
-        ;; set up dx register for disk loading
-        mov dh, 1               ; # sectors we want to load into memory
-        mov dl, 0               ; drive number to load (0=boot disk)
-
         ;; set up ES:BX memory address/segment:offset to load sector(s) into
         mov bx, 0x1000          ; load sector to memory address 0x1000
         mov es, bx              ; ES = 0x1000
-        mov bx, 0               ; ES:BX = 0x1000:0
+        mov bx, 0x0             ; ES:BX = 0x1000:0
 
-        call disk_load          ; loads our kernel into memory
+        ; setup disk read
+        mov dh, 0x0             ; head 0
+        mov dl, 0x0             ; drive 0
+        mov ch, 0x0             ; cylinder 0
+        mov cl, 0x02            ; starting sector to read
+
+read_disk:
+        mov ah, 0x02            ; BIOS int 13/ah=2 read disk sectors
+        mov al, 0x01            ; number of sectors to read
+        int 0x13                ; BIOS interrupts for disk functions
+
+        jc read_disk            ; retry if disk read error (carry flag set/ = 1)
 
         ;; reset segment registers for RAM
         mov ax,0x1000
@@ -24,10 +31,6 @@
 
         ; jmp es:bx               ; jump to wherever it's loaded
         jmp 0x1000:0x0            ; jump to wherever it's loaded
-
-        ;; include files
-        include 'disk_load.asm'
-        include 'print_string.asm'
 
         ;; Boot sector magic
         times 510-($-$$) db 0   ; pads out 0 untill we reach 510th byte
--- kernel.asm
+++ kernel.asm
@@ -1,7 +1,6 @@
 ;;
 ;; kernel.asm : Basic 'kernel' loaded from our bootsector
 ;;
-
         ; Set video mode
         mov ah, 0x00            ; Int 0x10/ ah 0x00 => set video mode
         mov al, 0x03            ; 80x25 test mode
@@ -16,14 +15,24 @@
         mov si, teststring      ; moving memory address at teststring into si register
         call print_string
 
-        ; mov dx, 0x12AB          ; sample hex number to print
-        ; call print_hex
-
         ;; End pgm
-        ; hlt                     ; halt the cpu
+        hlt                     ; halt the cpu
 
-        include 'print_string.asm'
-        include 'print_hex.asm'
+print_string:
+        mov ah, 0x0e            ; int 10/ ah Bios teletype output
+        mov bh, 0x0             ; page number
+        mov bl, 0x07            ; color
+
+print_char:
+        mov al, [si]            ; move charecter value at address in si into al
+        cmp al, 0
+        je end_print              ; jump if equal (al == 0) to halt label
+        int 0x10                ; Call for BIOS video interrupt
+        add si, 1               ; Move address to next charecter
+        jmp print_char        ; Jump back to the loop, and continue
+
+end_print:
+        ret
 
 teststring:         db 'Kernel booted, Welcome to SourceOS!', 0xd, 0xa, 0   ; 0/null to null terminate ; 0xd carriage return `\r` & 0xa newline `\n`
 
