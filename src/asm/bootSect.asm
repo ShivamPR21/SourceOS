c112cbf39823d4d9cec48937b410b8ba6bbdf44a
ShivamPR21 <pandeyshivam2023robotics@gmail.com>
2023-11-29 02:20:33 +0530

    Simple kernel file read complete.
    Signed-off-by: ShivamPR21 <pandeyshivam2023robotics@gmail.com>

--- src/asm/bootSect.asm
+++ src/asm/bootSect.asm
@@ -3,6 +3,7 @@
 ;;
         org 0x7c00              ; Indicated where Boot code starts; makes sure adresses don't change
 
+        ;; READ FILE TABLE INTO MEMORY FIRST
         ;; set up ES:BX memory address/segment:offset to load sector(s) into
         mov bx, 0x1000          ; load sector to memory address 0x1000
         mov es, bx              ; ES = 0x1000
@@ -14,15 +15,34 @@
         mov ch, 0x0             ; cylinder 0
         mov cl, 0x02            ; starting sector to read
 
-read_disk:
+read_disk1:
         mov ah, 0x02            ; BIOS int 13/ah=2 read disk sectors
         mov al, 0x01            ; number of sectors to read
         int 0x13                ; BIOS interrupts for disk functions
 
-        jc read_disk            ; retry if disk read error (carry flag set/ = 1)
+        jc read_disk1            ; retry if disk read error (carry flag set/ = 1)
+
+        ;; READ KERNEL INTO MEMORY SECOND
+        ;; set up ES:BX memory address/segment:offset to load sector(s) into
+        mov bx, 0x2000          ; load sector to memory address 0x2000
+        mov es, bx              ; ES = 0x1000
+        mov bx, 0x0             ; ES:BX = 0x1000:0
+
+        ; setup disk read
+        mov dh, 0x0             ; head 0
+        mov dl, 0x0             ; drive 0
+        mov ch, 0x0             ; cylinder 0
+        mov cl, 0x03            ; starting sector to read
+
+read_disk2:
+        mov ah, 0x02            ; BIOS int 13/ah=2 read disk sectors
+        mov al, 0x01            ; number of sectors to read
+        int 0x13                ; BIOS interrupts for disk functions
+
+        jc read_disk2            ; retry if disk read error (carry flag set/ = 1)
 
         ;; reset segment registers for RAM
-        mov ax,0x1000
+        mov ax,0x2000
         mov ds, ax                ; data segment
         mov es, ax                ; extra segment
         mov fs, ax                ; ""
@@ -30,7 +50,7 @@ read_disk:
         mov ss, ax                ; stack segment
 
         ; jmp es:bx               ; jump to wherever it's loaded
-        jmp 0x1000:0x0            ; jump to wherever it's loaded
+        jmp 0x2000:0x0            ; jump to wherever it's loaded
 
         ;; Boot sector magic
         times 510-($-$$) db 0   ; pads out 0 untill we reach 510th byte
--- src/asm/kernel.asm
+++ src/asm/kernel.asm
@@ -12,10 +12,49 @@
         mov bl, 0x01
         int 0x10
 
-        mov si, teststring      ; moving memory address at teststring into si register
+        ;; Print screen heading and menue options
+        mov si, menuestring      ; moving memory address at teststring into si register
         call print_string
 
+        ;; Get user input print to screen and choose a menue option or run command
+        push di                 ; Store di location to stack, to get user input command
+
+get_input:
+        mov di, cmdString       ; move location of command string to di
+
+keyloop:
+        mov ax, 0x00            ; ah = 0x00 and al = 0x00
+        int 0x16                  ; BIOS int get keystroke ah=00, character goes into al
+
+        mov ah, 0x0e
+        mov bh, 0x00
+        cmp al, 0xD             ; Did user presss 'Enter' key
+        je run_cmd
+        int 0x10                ; Print input character to screen
+        mov [di], al            ; move command into [di]
+        add di, 1               ; move to next byte
+        jmp keyloop             ; Loop through keyloop
+
+run_cmd:
+        ; pop di                  ; restore di from stack
+        mov byte [di], 0        ; null terminate cndString from di
+        mov al, [cmdString]
+        cmp al, 'F'           ; file table command/menue option
+        jne not_found
+        cmp al, 'N'             ; end current program
+        je end_command
+        mov si, success         ; command found!
+        call print_string
+        jmp get_input
+
+not_found:
+        mov si, failure
+        call print_string
+        jmp get_input
+
+end_command:
         ;; End pgm
+        cli                     ; clear int
         hlt                     ; halt the cpu
 
 print_string:
@@ -34,7 +73,14 @@ print_char:
 end_print:
         ret
 
-teststring:         db 'Kernel booted, Welcome to SourceOS!', 0xd, 0xa, 0   ; 0/null to null terminate ; 0xd carriage return `\r` & 0xa newline `\n`
+menuestring:         db 'Kernel booted, Welcome to SourceOS!'   ; 0/null to null terminate ; 0xd carriage return `\r` & 0xa newline `\n`
+                        ; times 80 - ($ - $$) db '-'
+                        db 0xa, 0xd, 0xa, 0xd
+                        db 'F> File & Program Broswer/Loader', 0xa, 0xd, 0
+
+success:        db 'Command ran successfully!', 0
+failure:        db 'Ooops! Something went wrong :(', 0
+cmdString:      db ''
 
        ;; Sector padding magic
         times 512-($-$$) db 0   ; pads out 0 untill we reach 512th byte
